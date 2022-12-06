b098967cdad7cc90b31373b73ef3c127e3f0d7a1
Ando Emerencia <a.c.emerencia@researchable.nl>
2022-12-06 18:03:31 +0100

    feat(aoc): day six

---
+++ 06/b/assignment.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <utility>
+#include <climits>
+#include <limits>
+#include <cstring>
+#include <numeric>
+#include <cstdio>
+
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+const int WINDOW_SIZE = 14;
+
+class Assignment {
+public:
+
+  bool isAllDifferent(vector<char>& window) {
+    set<char> windowSet(window.begin(), window.end());
+    bool result = window.size() == windowSet.size();
+    return result;
+  }
+
+  void printWindow(vector<char>& window) {
+    for (int i = 0; i < window.size(); i++) {
+      cout << window[i];
+    }
+    cout << endl;
+  }
+
+  int startOfPacket(string line) {
+    vector<char> window(WINDOW_SIZE);
+    int curIdx = 0;
+    for (int i = 0;i < line.size();i++) {
+      window[curIdx] = line[i];
+      if (i > WINDOW_SIZE - 2) {
+        if (isAllDifferent(window)) {
+          return i + 1;
+        }
+      }
+      curIdx = (1 + curIdx)%WINDOW_SIZE;
+    }
+    return -1;
+  }
+  void solution() {
+    while (cin.good()) {
+      string line;
+      getline(cin, line);
+      if (line.empty()) continue;
+      cout << startOfPacket(line) << endl;
+    }
+  };
+};
+
+int main() {
+  Assignment obj;
+  obj.solution();
+}
