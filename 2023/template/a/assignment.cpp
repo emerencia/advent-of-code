2fa3c45d66a7fea691ce289bc2fabb028007e038
Ando Emerencia <a.c.emerencia@researchable.nl>
2023-12-01 16:09:29 +0100

    refactor(template): remake template for 2023

---
+++ 2023/template/a/assignment.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <utility>
+#include <climits>
+#include <limits>
+#include <cstring>
+#include <numeric>
+#include <cstdio>
+
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+class Assignment {
+public:
+
+  int solution() {
+    int result = 0;
+    while (cin.good()) {
+      string line;
+      getline(cin, line);
+      if (line.empty()) continue;
+
+      // DO STUFF
+    }
+    return result;
+  }
+};
+
+int main() {
+  Assignment obj;
+  cout << obj.solution() << endl;
+}
