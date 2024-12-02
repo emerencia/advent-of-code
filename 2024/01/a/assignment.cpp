d2526ee5c8ecc218c5e86ae3dea3add3b5311bb4
Ando Emerencia <a.c.emerencia@researchable.nl>
2024-12-02 12:39:30 +0100

    feat(aoc): day 1

---
+++ 2024/01/a/assignment.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <utility>
+#include <climits>
+#include <limits>
+#include <cstring>
+#include <numeric>
+#include <cstdio>
+
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+class Assignment {
+public:
+
+  int solution() {
+    int result = 0;
+    vector<int> left;
+    vector<int> right;
+    while (cin.good()) {
+      string line;
+      getline(cin, line);
+      if (line.empty()) continue;
+
+      stringstream ss(line);
+      int x, y;
+      ss >> x >> y;
+      left.push_back(x);
+      right.push_back(y);
+    }
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+    for (int i = 0; i < left.size(); ++i) {
+      result += abs(left[i] - right[i]);
+    }
+    return result;
+  }
+};
+
+int main() {
+  Assignment obj;
+  cout << obj.solution() << endl;
+}
---
+++ 2024/01/b/assignment.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <utility>
+#include <climits>
+#include <limits>
+#include <cstring>
+#include <numeric>
+#include <cstdio>
+
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+class Assignment {
+public:
+  static int solution() {
+    int result = 0;
+    vector<int> left;
+    map<int, int> right;
+    while (cin.good()) {
+      string line;
+      getline(cin, line);
+      if (line.empty()) continue;
+
+      stringstream ss(line);
+      int x, y;
+      ss >> x >> y;
+      left.push_back(x);
+      if (right.find(y) == right.end()) {
+        right[y] = 1;
+      } else {
+        right[y]++;
+      }
+    }
+    for (int i : left) {
+      if (right.find(i) != right.end()) {
+        result += i * right[i];
+      }
+    }
+    return result;
+  }
+};
+
+int main() {
+  cout << Assignment::solution() << endl;
+}
