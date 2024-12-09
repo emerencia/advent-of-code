03627fb9fd7b8335ae13dcaf798ba809b98ba56d
Ando Emerencia <a.c.emerencia@researchable.nl>
2024-12-09 18:51:31 +0100

    feat(aoc): day 5

---
+++ 2024/05/b/assignment.cpp
@@ -0,0 +1,91 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <utility>
+#include <climits>
+#include <limits>
+#include <cstring>
+#include <numeric>
+#include <cstdio>
+
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+class Assignment {
+public:
+  map<int, vector<int> > before;
+  map<int, vector<int> > after;
+
+  bool validUpdate(vector<int> &update) {
+    bool swapped = false;
+    for (int i = 0; i < update.size(); ++i) {
+      int nbefore = update[i];
+      for (int j = i + 1; j < update.size(); ++j) {
+        int nafter = update[j];
+        if (find(after[nafter].begin(), after[nafter].end(), nbefore) != after[nafter].end()) {
+          swap(update[i], update[j]);
+          i = -1;
+          swapped = true;
+          break;
+        }
+        if (find(before[nbefore].begin(), before[nbefore].end(), nafter) != before[nbefore].end()) {
+          swap(update[i], update[j]);
+          i = -1;
+          swapped = true;
+          break;
+        }
+      }
+    }
+    return swapped;
+  }
+
+  int solution() {
+    int result = 0;
+    before.clear();
+    after.clear();
+    while (cin.good()) {
+      string line;
+      getline(cin, line);
+      if (line.empty()) break;
+
+      int nbefore, nafter;;
+      sscanf(line.c_str(), "%d|%d", &nbefore, &nafter);
+
+      before[nafter].push_back(nbefore);
+      after[nbefore].push_back(nafter);
+    }
+
+    while (cin.good()) {
+      string line;
+      getline(cin, line);
+      if (line.empty()) continue;
+
+      vector<int> update;
+      stringstream ss(line);
+      int n;
+      char c;
+      while (ss >> n) {
+        update.push_back(n);
+        ss >> c;
+      }
+      if (validUpdate(update)) {
+        result += update[update.size() / 2];
+      }
+    }
+    return result;
+  }
+};
+
+int main() {
+  Assignment obj;
+  cout << obj.solution() << endl;
+}
--- 2024/README.md
+++ 2024/README.md
@@ -2,4 +2,6 @@
 
 - 01: historian hysteria: sorting arrays and counting.
 - 02: red-nosed reports: nested for-loop. running through an array, keeping a state, and comparing prev/cur values.
-- 03: write a parser.
+- 04: ceres search: navigate in a grid.
+- 03: mull it over: write a parser.
+- 05: print queue: input parsing, maps, and swaps in vectors.
